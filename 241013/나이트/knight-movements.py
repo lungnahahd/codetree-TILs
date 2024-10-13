5adc5c9ee9c2d4bab555e84b28d63e511b46430c
Lungnaha <67555400+lungnahahd@users.noreply.github.com>
2024-10-13 19:50:00 +0900

    Title: 나이트, Time: 176ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241013/나이트/knight-movements.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+
+
+size = int(input())
+start_r, start_c, end_r, end_c = list(map(int, input().split()))
+start_r, start_c, end_r, end_c = start_r-1, start_c-1, end_r-1, end_c-1
+
+location = [[0 for _ in range(size)] for _ in range(size)]
+location[start_r][start_c] = 1
+
+move_r = [-1,-2,-2,-1,+1,+2,+2,+1]
+move_c = [-2,-1,+1,+2,-2,-1,+1,+2]
+
+answer = 0
+def bfs(r,c):
+    global answer
+    visited = [[False for _ in range(size)] for _ in range(size)]
+    visited[r][c] = True
+    save = deque([(r,c, 0)])
+    while save:
+        now_r, now_c, now_val = save.popleft()
+        if now_r == end_r and now_c == end_c:
+            answer = max(now_val, answer)
+            return
+        for m_idx in range(8):
+            next_r, next_c = now_r + move_r[m_idx], now_c + move_c[m_idx]
+            if 0 <= next_r < size and 0 <= next_c < size:
+                if not visited[next_r][next_c]:
+                    visited[next_r][next_c] = True
+                    save.append((next_r,next_c,now_val+1))
+bfs(start_r, start_c)
+print(answer)
