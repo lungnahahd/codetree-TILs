cb72cff937b50184969430f084056617dda1a32e
Lungnaha <67555400+lungnahahd@users.noreply.github.com>
2024-10-13 18:48:19 +0900

    Title: xor 결과 최대 만들기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241013/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,19 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+num_cnt, choice_cnt = list(map(int, input().split()))
+nums = list(map(int, input().split()))
+
+
+
+choice_nums = list(combinations(nums, 3))
+
+answer = 0
+for now in choice_nums:
+    temp = now[0]
+    for num in now[1:]:
+        temp = temp^num
+    answer = max(answer, temp)
+
+print(answer)
