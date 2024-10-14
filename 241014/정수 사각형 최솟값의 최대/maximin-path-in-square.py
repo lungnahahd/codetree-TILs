1dde6a8becc562e2f6f1f788887affd8b5baf14a
Lungnaha <67555400+lungnahahd@users.noreply.github.com>
2024-10-14 12:01:21 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241014/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 241014/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -7,24 +7,18 @@ for _ in range(size):
     temp = list(map(int, input().split()))
     location.append(temp)
 
+INT_MAX = sys.maxsize
 move_r = [+1,0]
 move_c = [0,+1]
+visited = [[False for _ in range(size)] for _ in range(size)]
+dp = [[INT_MAX for _ in range(size+1)] for _ in range(size+1)]
 
 
 answer = 0
-def dfs(r, c, result):
-    global answer
 
-    if r == size-1 and c == size-1:
-        answer = max(answer, result)
-        return
-    for m_idx in range(2):
-        next_r, next_c = move_r[m_idx]+r, move_c[m_idx]+c
-        if 0 <= next_r < size and 0 <= next_c < size:
-            if result > location[next_r][next_c]:
-                dfs(next_r, next_c, location[next_r][next_c])
-            else:
-                dfs(next_r, next_c, result)
+for r in range(size+1):
+    for c in range(size+1):
+        dp[r][c] = min(location[r-1][c-1], max(dp[r-1][c], dp[r][c-1]))
 
-dfs(0,0,location[0][0])
-print(answer)
+
+print(dp[size][size])
