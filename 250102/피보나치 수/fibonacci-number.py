3cb86c5f31356c6c9593f5ccf9c359521992f31f
Lungnaha <67555400+lungnahahd@users.noreply.github.com>
2025-01-02 06:19:39 +0900

    Title: 피보나치 수, Time: 39ms, Memory: 16MB, Status: Passed - CodeTree

--- 250102/피보나치 수/fibonacci-number.py
+++ 250102/피보나치 수/fibonacci-number.py
@@ -3,12 +3,10 @@ input= sys.stdin.readline
 
 n = int(input())
 
-fibo = [0 for _ in range(n+1)]
-fibo[1] = 1
-fibo[2] = 1
-
+fibo = [0,1,1]
 if(n > 2):
     for idx in range(3,n+1):
-        fibo[idx] = fibo[idx-1] + fibo[idx-2]
+        val = fibo[idx-1] + fibo[idx-2]
+        fibo.append(val)
 
 print(fibo[n])
