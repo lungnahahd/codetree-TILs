3439ecb4302de2f7349a6c8c3b730cc77b63a794
Lungnaha <67555400+lungnahahd@users.noreply.github.com>
2025-01-03 00:21:11 +0900

    Title: 계단 오르기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250103/계단 오르기/climbing-stairs.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+step = int(input())
+
+answer = [0,0,1,1,1]
+
+if step < 5:
+    print(answer[step])
+else:
+    for idx in range(5,step+1):
+        jump = answer[idx-3] + answer[idx-2]
+        answer.append(jump)
+    print(answer[step])
